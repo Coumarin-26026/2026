42fedf03cc3cecf6de16d4d140623c35e5ce2f17
Coumarin-26026 <thanh02062k2@gmail.com>
2026-06-20 23:27:24 +0700

    Update build-mac.sh

--- .ci/build-mac.sh
+++ .ci/build-mac.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -ex
 
-# Gather explicit version number and number of commits
+##############################################################################
+
+# Version info
+
+##############################################################################
 
 COMM_TAG=$(awk '/version{.*}/ { printf("%d.%d.%d", $5, $6, $7) }' rpcs3/rpcs3_version.cpp)
 COMM_COUNT=$(git rev-list --count HEAD)
@@ -11,14 +15,16 @@ export LVER="${COMM_TAG}-${COMM_COUNT}-${COMM_HASH}"
 
 echo "AVVER=$AVVER" >> .ci/ci-vars.env
 
-# Homebrew
+##############################################################################
+
+# Homebrew setup
+
+##############################################################################
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
-
-# Install dependencies
 
 brew install -f --overwrite --quiet 
 ccache 
@@ -43,8 +49,23 @@ qtsvg
 qtdeclarative 
 protobuf || true
 
+##############################################################################
+
+# Compiler
+
+##############################################################################
+
 export CC=clang
 export CXX=clang++
+
+##############################################################################
+
+# Paths
+
+##############################################################################
+
+export WORKDIR
+WORKDIR="$(pwd)"
 
 export BREW_PATH
 BREW_PATH="$(brew --prefix)"
@@ -52,23 +73,29 @@ BREW_PATH="$(brew --prefix)"
 export BREW_BIN="$BREW_PATH/bin"
 export BREW_SBIN="$BREW_PATH/sbin"
 
-export WORKDIR
-WORKDIR="$(pwd)"
+##############################################################################
 
 # ccache
+
+##############################################################################
 
 if [ ! -d "$CCACHE_DIR" ]; then
 mkdir -p "$CCACHE_DIR"
 fi
 
-# Build Qt from source
+##############################################################################
+
+# Build Qt dependencies
+
+##############################################################################
 
 export DEPS_DIR="$WORKDIR/deps"
 
 chmod +x .ci/build-dependencies-universal.sh
 
 if [ ! -f "$DEPS_DIR/lib/cmake/Qt6/Qt6Config.cmake" ]; then
-echo "Qt not found. Building dependencies..."
+echo "Qt6 not found"
+echo "Building dependencies"
 
 ```
 export BUILD_FFMPEG=0
@@ -78,22 +105,46 @@ export BUILD_FFMPEG=0
 
 fi
 
-# Toolchain
+##############################################################################
+
+# Qt
+
+##############################################################################
 
 export Qt6_DIR="$DEPS_DIR/lib/cmake/Qt6"
 export CMAKE_PREFIX_PATH="$DEPS_DIR"
 
+##############################################################################
+
+# SDL
+
+##############################################################################
+
 export SDL3_DIR="$BREW_PATH/opt/sdl3/lib/cmake/SDL3"
 
+##############################################################################
+
+# LLVM
+
+##############################################################################
+
 export PATH="$BREW_PATH/opt/llvm@$LLVM_COMPILER_VER/bin:$PATH"
+
+export LLVM_DIR="$BREW_PATH/opt/llvm@$LLVM_COMPILER_VER"
 
 export LDFLAGS="-L$BREW_PATH/opt/llvm@$LLVM_COMPILER_VER/lib/c++ 
 -L$BREW_PATH/opt/llvm@$LLVM_COMPILER_VER/lib/unwind 
 -lunwind"
 
+##############################################################################
+
 # Vulkan
 
+##############################################################################
+
 export VULKAN_SDK="$BREW_PATH/opt/molten-vk"
+
+mkdir -p "$VULKAN_SDK/lib"
 
 if [ ! -e "$VULKAN_SDK/lib/libvulkan.dylib" ]; then
 ln -sf 
@@ -101,50 +152,68 @@ ln -sf
 "$VULKAN_SDK/lib/libvulkan.dylib"
 fi
 
-# LLVM
-
-export LLVM_DIR="$BREW_PATH/opt/llvm@$LLVM_COMPILER_VER"
+##############################################################################
 
 # Submodules
+
+##############################################################################
 
 git submodule -q update 
 --init 
 --depth=1 
 --jobs=8 
-$(awk '/path/ && !/llvm/ && !/opencv/ && !/SDL/ && !/feralinteractive/ { print $3 }' .gitmodules)
+$(awk '
+/path/ &&
+!/llvm/ &&
+!/opencv/ &&
+!/SDL/ &&
+!/feralinteractive/
+{ print $3 }
+' .gitmodules)
+
+##############################################################################
 
 # Build
 
+##############################################################################
+
 rm -rf build
+
 mkdir build
 cd build
 
-cmake .. \
--DCMAKE_PREFIX_PATH="$DEPS_DIR" \
--DBUILD_RPCS3_TESTS="${RUN_UNIT_TESTS}" \
--DRUN_RPCS3_TESTS="${RUN_UNIT_TESTS}" \
--DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
--DCMAKE_OSX_SYSROOT="$(xcrun --sdk macosx --show-sdk-path)" \
--DMACOSX_BUNDLE_SHORT_VERSION_STRING="${COMM_TAG}" \
--DMACOSX_BUNDLE_BUNDLE_VERSION="${COMM_COUNT}" \
--DSTATIC_LINK_LLVM=ON \
--DUSE_SDL=ON \
--DUSE_DISCORD_RPC=ON \
--DUSE_AUDIOUNIT=ON \
--DUSE_SYSTEM_FFMPEG=OFF \
--DUSE_NATIVE_INSTRUCTIONS=OFF \
--DUSE_PRECOMPILED_HEADERS=OFF \
--DUSE_SYSTEM_MVK=ON \
--DUSE_SYSTEM_SDL=ON \
--DUSE_SYSTEM_OPENCV=ON \
+cmake .. 
+-DCMAKE_PREFIX_PATH="$DEPS_DIR" 
+-DBUILD_RPCS3_TESTS="${RUN_UNIT_TESTS}" 
+-DRUN_RPCS3_TESTS="${RUN_UNIT_TESTS}" 
+-DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 
+-DCMAKE_OSX_SYSROOT="$(xcrun --sdk macosx --show-sdk-path)" 
+-DMACOSX_BUNDLE_SHORT_VERSION_STRING="${COMM_TAG}" 
+-DMACOSX_BUNDLE_BUNDLE_VERSION="${COMM_COUNT}" 
+-DSTATIC_LINK_LLVM=ON 
+-DUSE_SDL=ON 
+-DUSE_DISCORD_RPC=ON 
+-DUSE_AUDIOUNIT=ON 
+-DUSE_SYSTEM_FFMPEG=OFF 
+-DUSE_NATIVE_INSTRUCTIONS=OFF 
+-DUSE_PRECOMPILED_HEADERS=OFF 
+-DUSE_SYSTEM_MVK=ON 
+-DUSE_SYSTEM_SDL=ON 
+-DUSE_SYSTEM_OPENCV=ON 
 -G Ninja
 
 ninja -j$(sysctl -n hw.logicalcpu)
 
-build_status=$?
+BUILD_STATUS=$?
 
 cd ..
 
-if [ "$build_status" -eq 0 ]; then
+##############################################################################
+
+# Deploy
+
+##############################################################################
+
+if [ "$BUILD_STATUS" -eq 0 ]; then
 .ci/deploy-mac.sh
 fi
